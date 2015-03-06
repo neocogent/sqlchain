6d19d089881a398e6a915346e9af019f7d09de06
tkhaew <topherkhaew@gmail.com>
2015-03-07 02:51:27 +0700

    add coinbase,scripts,hdr data

--- bitcoin.sql
+++ bitcoin.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `address` (`id` INTEGER PRIMARY KEY NOT NULL , `addr` BLOB not null) ;
-CREATE TABLE `blocks` (`id` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL ,`height` INTEGER NOT NULL ,`orphan` BOOL NOT NULL ,`hash` BLOB NOT NULL ,`time` INTEGER NOT NULL  );
+CREATE TABLE `blocks` (`id` INTEGER PRIMARY KEY NOT NULL ,`hash` BLOB NOT NULL ,`time` INTEGER NOT NULL  );
 CREATE TABLE `outputs` (`id` INTEGER PRIMARY KEY NOT NULL ,`value` INTEGER,`addr_id` INTEGER );
-CREATE TABLE `scripts` (`out_id` INTEGER PRIMARY KEY  NOT NULL ,`script` BLOB);
+CREATE TABLE `scripts` (`out_id` INTEGER PRIMARY KEY  NOT NULL ,`script_in` BLOB,`script_out` BLOB);
 CREATE TABLE `trxs` (`id` INTEGER PRIMARY KEY NOT NULL , `hash` BLOB NOT NULL , `inputs` BLOB, `outputs` BLOB, `block_id` INTEGER);
+CREATE TABLE `hdrs` (`id` INTEGER PRIMARY KEY NOT NULL , `vers` INTEGER, `prevhash` BLOB, `merkle` BLOB, `nbits` INTEGER, `nonce` INTEGER);
