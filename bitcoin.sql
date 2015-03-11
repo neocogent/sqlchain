0a1ad52303d75bcf3d62d14a64e38e74275a7a64
tkhaew <topherkhaew@gmail.com>
2015-03-12 01:27:32 +0700

    add block verify

--- bitcoin.sql
+++ bitcoin.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `address` (`id` INTEGER PRIMARY KEY NOT NULL , `addr` BLOB not null) ;
-CREATE TABLE `blocks` (`id` INTEGER PRIMARY KEY NOT NULL ,`hash` BLOB NOT NULL ,`time` INTEGER NOT NULL  );
+CREATE TABLE `blocks` (`id` INTEGER PRIMARY KEY NOT NULL ,`hash` BLOB NOT NULL ,`hdr` BLOB NOT NULL , "coinbase" BLOB );
 CREATE TABLE `outputs` (`id` INTEGER PRIMARY KEY NOT NULL ,`value` INTEGER,`addr_id` INTEGER );
-CREATE TABLE `scripts` (`out_id` INTEGER PRIMARY KEY  NOT NULL ,`script_in` BLOB,`script_out` BLOB);
+CREATE TABLE `scripts` (`out_id` INTEGER PRIMARY KEY  NOT NULL ,"sig" BLOB DEFAULT (null) ,"pk" BLOB DEFAULT (null) );
 CREATE TABLE `trxs` (`id` INTEGER PRIMARY KEY NOT NULL , `hash` BLOB NOT NULL , `inputs` BLOB, `outputs` BLOB, `block_id` INTEGER);
-CREATE TABLE `hdrs` (`id` INTEGER PRIMARY KEY NOT NULL , `vers` INTEGER, `prevhash` BLOB, `merkle` BLOB, `nbits` INTEGER, `nonce` INTEGER);
+
