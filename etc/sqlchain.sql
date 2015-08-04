0b3df05b3fd6bcc5c6fa31601cd7de5e786a28b6
neocogent <info@neocogent.com>
2015-08-04 13:34:03 +0700

    fill in more www docs

--- etc/sqlchain.sql
+++ etc/sqlchain.sql
@@ -10,13 +10,13 @@ CREATE TABLE `blocks` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 
 CREATE TABLE `address` (
-  `id` decimal(13,0) NOT NULL,
+  `id` decimal(13) NOT NULL,
   `addr` binary(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 
 CREATE TABLE `trxs` (
-  `id` decimal(13,0) NOT NULL,
+  `id` decimal(13) NOT NULL,
   `hash` binary(37) NOT NULL,
   `txdata` decimal(13,0) DEFAULT NULL,
   `block_id` int(11) DEFAULT NULL,
@@ -25,16 +25,16 @@ CREATE TABLE `trxs` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 
 CREATE TABLE `outputs` (
-  `id` decimal(16,0) NOT NULL,
-  `value` decimal(16,0) DEFAULT NULL,
-  `addr_id` decimal(13,0) DEFAULT NULL,
-  `tx_id` decimal(13,0) DEFAULT NULL,
+  `id` decimal(16) NOT NULL,
+  `value` decimal(16) DEFAULT NULL,
+  `addr_id` decimal(13) DEFAULT NULL,
+  `tx_id` decimal(13) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `addr` (`addr_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 
 CREATE TABLE `mempool` (
-  `id` decimal(13,0) NOT NULL,
+  `id` decimal(13) NOT NULL,
   `sync_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `sync` (`sync_id`)
