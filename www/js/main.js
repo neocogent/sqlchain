1539538356c8d2cc23a425b23e04d8953672b150
neocogent <info@neocogent.com>
2015-08-08 14:51:50 +0700

    web demo, /api rework

--- www/js/main.js
+++ www/js/main.js
@@ -2,18 +2,33 @@
     var id = 0;
 
     function apiClk(e) {
-        var url = $(this);
-        if(url.next().hasClass('resp'))
-            url.next().remove();
-        else
-            $.get(url.text().split('[')[0], function( data ) {
-                var msg = $('#blank').clone().attr('id', '#d'+id).insertAfter(url);
-                msg.html(url.hasClass('raw') ? data : "<pre>"+JSON.stringify(data, null, 2)+"</pre>");
+        var url='', obj = $(this);
+        e.stopPropagation();
+        if(obj.hasClass('opt'))
+            url = obj.text();
+        if(obj.is('span')) {
+            obj = obj.parent();
+            url = obj.text().split('?')[0] + url;
+        }
+        var objIns = obj.next().hasClass('info') ? obj.next() : obj;
+        if($(this).is( 'li' )) {
+            if(obj.next().hasClass('info')) 
+                obj.next().toggle();
+            if(obj.hasClass('noargs'))
+                url = obj.text().split('?')[0]
+            }
+        if(objIns.next().hasClass('resp'))
+            objIns.next().remove();
+        else {
+            $.get(url, function( data ) {
+                var msg = $('#blank').clone().attr('id', '#d'+id).insertAfter(objIns);
+                msg.html(obj.hasClass('raw') ? data : "<pre>"+JSON.stringify(data, null, 2)+"</pre>");
                 id += 1;
-                }, "json");       
+                }, "json");
+            }
     }
-
     $(document).ready( function() {
         $('.apiClk li').click(apiClk);
+        $('.apiClk span').click(apiClk);
     });
 })(jQuery);
