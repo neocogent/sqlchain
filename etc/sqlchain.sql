d30260600dbe7a6c0727eb31c6f849c37f50e532
neocogent <info@neocogent.com>
2015-08-11 04:06:23 +0700

    fixes, extend api, sync code

--- etc/sqlchain.sql
+++ etc/sqlchain.sql
@@ -17,9 +17,12 @@ CREATE TABLE `address` (
 
 CREATE TABLE `trxs` (
   `id` decimal(13) NOT NULL,
-  `hash` binary(37) NOT NULL,
+  `hash` binary(32) NOT NULL,
+  `ins` tinyint unsigned NOT NULL,
+  `outs` tinyint unsigned NOT NULL,
+  `txsize` smallint unsigned NOT NULL,
   `txdata` decimal(13,0) DEFAULT NULL,
-  `block_id` int(11) DEFAULT NULL,
+  `block_id` decimal(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `block` (`block_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -38,5 +41,14 @@ CREATE TABLE `mempool` (
   `sync_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `sync` (`sync_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `orphans` (
+  `sync_id` int(11) NOT NULL,
+  `block_id` int(11) NOT NULL,
+  `hash` binary(32) NOT NULL,
+  `hdr` binary(80) NOT NULL,
+  `coinbase` varbinary(100) NOT NULL,  
+  KEY (`sync_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
