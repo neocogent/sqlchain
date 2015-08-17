a4eaeef9d5e99dfaa0d7735575a39355ff014110
neocogent <info@neocogent.com>
2015-08-18 03:29:28 +0700

    update blkdat with help info

--- etc/collisions.sql
+++ etc/collisions.sql
@@ -9,3 +9,8 @@ select  count(*) from address where  cast(conv(hex(reverse(unhex(substr(sha2(add
 # Make addr_id in SQL
 
 select id,hex(addr),cast(conv(hex(reverse(unhex(substr(sha2(addr,0),1,10)))),16,10)*2 as unsigned) as nid from address limit 20;
+
+
+# Find missing rows (gaps) in sequence.
+
+SELECT t1.id-1 FROM blkdat t1 LEFT OUTER JOIN blkdat t2 ON t2.id=t1.id-1 WHERE t2.id IS NULL AND t1.id > 0 ORDER BY t1.id;
