da63c95b8c2629a6a471cf82ecad15a292599647
neocogent <info@neocogent.com>
2017-12-03 21:36:03 +0700

    add title subst on demo

--- sqlchain/democvt.py
+++ sqlchain/democvt.py
@@ -10,6 +10,7 @@ import os, sys
 
 demos = {
     'bitcoin': {
+        'text':['Bitcoin SQL Blockchain Explorer'],
         'height':['123432','167324'],
         'blockhash':['0000000000001271efd5d9f7e539909160a181b2c0a2b8c164d6f8159e5c7dd9'],
         'addr':['1JK6pUCAXfnvcbXEpdVSxhVZ8W6kxQ4VEH','1FvzCLoTPGANNjWoUo6jUGuAG3wg1w4YjR','1CmTtsKEqPxZsW3YjGYXbPSY89xrzkhy94',
@@ -18,6 +19,7 @@ demos = {
                 '2acba2c6916cdfdbf3584dfdd32534af5031ab076029ff275167fa5181dee0a8'] },
 
     'testnet': {
+        'text':['Bitcoin Testnet SQL Blockchain Explorer'],
         'height':['892679','167324'],
         'blockhash':['0000000000003431c5b86d3788a953eeaeef2bd5b3491dff0f4e0a931aa4f8f3'],
         'addr':['muPawjrif323ikE2oJM3FbPxvTjGP1zNin','n4CLirZ5vrWxtNvEDDwkddN8g88TQk99WJ','n4CLirZ5vrWxtNvEDDwkddN8g88TQk99WJ',
@@ -26,6 +28,7 @@ demos = {
                 '1851c0248739bb3a71ae6d207748b35b9005d5cc31e2dc255c54a7278edc4c35'] },
 
     'litecoin': {
+        'text':['Litecoin SQL Blockchain Explorer'],    
         'height':['123432','167327'],
         'blockhash':['7fe250115bee18519dc0025bf9470c5d95838bce835b1796f5fd5e6371bd8328'],
         'addr':['LLv6ZfQzTeyiiSNALTpQmhK88Ty1XyYiWs','LLv6ZfQzTeyiiSNALTpQmhK88Ty1XyYiWs','LWfgd9WVdZk9xioJUCPgjQp2iJgbaekPTf',
@@ -34,6 +37,7 @@ demos = {
                 '799ed5d7fe40dc3af3199f785ad191e3c3059b2e3ccc65ef86dde011597ff595'] },
 
     'reddcoin': {
+        'text':['Reddcoin SQL Blockchain Explorer'],    
         'height':['123433','167324'],
         'blockhash':['554b176777f87f8b127be12dd467fe0484aa003254a617ad2200e7c4cc0a42b9'],
         'addr':['Ru4EobrFWYapv6MehxhF7eZBfRKFjAWqqf','RgQ8kpboZvAXAWcRUByfu5n3ZBHf6ZEkQ','Rk3asChtuHfnQfMibyNZxn6FELK3412uEK',
