78c8041d32a83b3e537f3a251f0b5184b7d1aecf
neocogent <info@neocogent.com>
2017-12-10 02:31:34 +0700

    fill in live tests, fixtures, db stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 import pytest
-def pytest_addoption(parser):
-    parser.addoption("--runlive", action="store_true",
-        help="run live tests")
 
+def pytest_addoption(parser):
+    parser.addoption("--runlive", action="store_true", help="run live tests")
+    parser.addoption("--server", action="store", default="localhost:8085", help="set api-server: host:port")
+
+@pytest.fixture
+def server(request):
+    return request.config.getoption("--server")
--- tests/test_live_api.py
+++ tests/test_live_api.py
@@ -5,15 +5,22 @@
 #     - compare json returned to livetest db json
 #
 
-import os, sys
-from struct import unpack
+import sys, time, requests
+
+try:
+    from deepdiff import DeepDiff
+except ImportError:
+    print "Cannot run database tests without deepdiff module"
 
 try:
     import sqlite3 as db
 except ImportError:
     print "Cannot run database tests without sqlite3 module"
-    
-import pytest   
+
+import pytest
+
+millis = lambda: int(round(time.time() * 1000))
+server = None
 
 live = pytest.mark.skipif(
     not pytest.config.getoption("--runlive"),
@@ -23,13 +30,88 @@ live = pytest.mark.skipif(
 # livetest db created by mklivetestdb.py
 @pytest.fixture(scope="module")
 def testdb(request):
+    global server
     if 'sqlite3' not in sys.modules:
         print "No test db available"
         return None
-    sql = db.connect('livetest.db')
+    server = request.config.getoption("--server")
+    cwd = str(request.fspath.join('..'))
+    sql = db.connect(cwd+'/livetest.db',isolation_level=None)
     cur = sql.cursor()
+    cur.execute("select name from sqlite_master where name='calls';")
+    if cur.fetchone() is None:
+        print "livetest.db not initialized."
+        return None
     return cur
 
+def api_call(url):
+    try:
+        call_ts = millis()
+        r = requests.get('http://'+server+url)
+        if r.status_code == requests.codes.ok:
+            return r.json(),millis()-call_ts
+    except requests.exceptions.ConnectionError:
+        return { "error": "Connect Error: http://"+server+url },0
+    return { "error": r.status_code },0
+
+def api_diff(cur, sqlstr, **kwargs):
+    cur.execute("select url,result from calls where url like ?;", (sqlstr,))
+    for url,result in cur:
+        rtn,rtt = api_call(url)
+        if 'error' in rtn:
+            return rtn
+        diff = DeepDiff(result, rtn, ignore_order=True, **kwargs)
+        cur.execute("insert into tests (url,result,diff,rtt) values (?,?,?,?);", (url,rtn,diff,rtt))
+        if diff != {}:
+            return diff
+    return True
+
 @live
-def test_live_api(testdb):
+def test_live_api_block(testdb):
+    if testdb is not None:
+        assert api_diff(testdb, '/block/%')
+
+@live
+def test_live_api_block_index(testdb):
+    if testdb is not None:
+        assert api_diff(testdb, '/block-index/%')
+
+@live
+def test_live_api_rawblock(testdb):
+    assert True
+
+@live
+def test_live_api_blocks(testdb): # not currently supported
+    assert True
+
+@live
+def test_live_api_tx(testdb):
+    assert True
+
+@live
+def test_live_api_rawtx(testdb):
+    assert True
+
+@live
+def test_live_api_addr(testdb):
+    assert True
+
+@live
+def test_live_api_utxo(testdb):
+    assert True
+
+@live
+def test_live_api_txs_block(testdb):
+    assert True
+
+@live
+def test_live_api_txs_addr(testdb):
+    assert True
+
+@live
+def test_live_api_addrs(testdb):
+    assert True
+
+@live
+def test_live_api_status(testdb):
     assert True
