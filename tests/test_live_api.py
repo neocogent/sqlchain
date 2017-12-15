4d771fa31d15e2df1ba8173ad767cf16d929bce1
neocogent <info@neocogent.com>
2017-12-15 19:45:43 +0700

    fix diff result as json obj

--- tests/test_live_api.py
+++ tests/test_live_api.py
@@ -6,7 +6,7 @@
 #     - compare json returned to livetest db json
 #
 
-import sys, time, requests
+import sys, time, requests, json
 
 try:
     from deepdiff import DeepDiff
@@ -62,7 +62,7 @@ def api_diff(cur, sqlstr, **kwargs):
         rtn,rtt = api_call(url)
         if 'error' in rtn:
             return rtn
-        diff = DeepDiff(result, rtn, ignore_order=True, **kwargs)
+        diff = DeepDiff(json.loads(result), rtn, ignore_order=True, **kwargs)
         cur.execute("insert into tests (url,result,diff,rtt) values (?,?,?,?);", (url,str(rtn),str(diff),rtt))
         if diff != {}:
             return diff
